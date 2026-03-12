ed5e88e55977866cdd24318a8e4c720e36092639
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2026-03-12 13:33:22 +0530

    Create Goals page with Navbar and content

---
+++ src/pages/goals.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+export default function Goals() {
+  return (
+    <div>
+      <Navbar />
+      <h1 style={{padding:"20px"}}>Goals</h1>
+      <p style={{padding:"20px"}}>Track your life goals.</p>
+    </div>
+  );
+}
