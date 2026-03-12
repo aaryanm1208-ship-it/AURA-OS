81c6c8ee74ebca8e3d0f26b75255a3f79c19ca63
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2026-03-12 13:38:55 +0530

    Create habits.js

---
+++ src/pages/habits.js
@@ -0,0 +1,29 @@
+import Navbar from "../components/Navbar"
+
+export default function Habits(){
+
+return(
+
+<div>
+
+<Navbar/>
+
+<div className="container">
+
+<h1>Habits</h1>
+
+<div className="card">
+
+<p>Exercise Daily</p>
+
+<p>Streak: 5 days</p>
+
+</div>
+
+</div>
+
+</div>
+
+)
+
+}
