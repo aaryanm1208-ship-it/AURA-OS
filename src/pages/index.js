e74f1167b3041dc5356277666be045855e0c9756
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2026-03-12 13:31:54 +0530

    Add Home component with Navbar and introductory text

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <div style={{textAlign:"center", marginTop:"100px"}}>
+        <h1>AURA OS</h1>
+        <p>Your Life. Organized. Optimized. Automated.</p>
+        <p>Created by Aaryan Anand Mudvikar</p>
+      </div>
+    </div>
+  );
+}
