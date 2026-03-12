e53ef04dd9d898b76bdeb03ee3937e4b21b14a78
Aaryan Anand Mudvikar <aaryanm1208@gmail.com>
2026-03-12 13:32:56 +0530

    Create Tasks page with Navbar and header

---
+++ src/pages/tasks.js
@@ -0,0 +1,11 @@
+import Navbar from "../components/Navbar";
+
+export default function Tasks() {
+  return (
+    <div>
+      <Navbar />
+      <h1 style={{padding:"20px"}}>Tasks</h1>
+      <p style={{padding:"20px"}}>Manage your daily tasks here.</p>
+    </div>
+  );
+}
